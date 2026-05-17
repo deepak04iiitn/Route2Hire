62a87e79bd71bc163026dc727f351cbadaba1a1d
Deepak Kumar Yadav <dky422003@gmail.comm>
2026-05-17 20:38:05 +0530

    updated the firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,3 +1,20 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "careerconnect-85965.firebaseapp.com",
+//   projectId: "careerconnect-85965",
+//   storageBucket: "careerconnect-85965.appspot.com",
+//   messagingSenderId: "865595243103",
+//   appId: "1:865595243103:web:cca6330c437416304679ea"
+// };
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,13 +22,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyAVm2xpbUscbiYq2RBrLu478nlZfNTzWc0",
   authDomain: "careerconnect-85965.firebaseapp.com",
   projectId: "careerconnect-85965",
-  storageBucket: "careerconnect-85965.appspot.com",
+  storageBucket: "careerconnect-85965.firebasestorage.app",
   messagingSenderId: "865595243103",
-  appId: "1:865595243103:web:cca6330c437416304679ea"
+  appId: "1:865595243103:web:f6fa600c83beb8334679ea"
 };
 
 // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+
+
